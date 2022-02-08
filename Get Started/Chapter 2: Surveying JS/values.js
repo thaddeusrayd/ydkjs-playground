6f34df31d42e7cbf289f311ada86c9de3c4e8429
thaddeusrayd <thaddeusrayd@gmail.com>
2022-02-08 17:43:09 -0600

    add boolean and number examples to values.js

--- Get Started/Chapter 2: Surveying JS/values.js
+++ Get Started/Chapter 2: Surveying JS/values.js
@@ -6,6 +6,12 @@
 
 const firstName = "Thad";
 console.log(`My name is ${firstName}.`); // My name is Thad.
-console.log("My name is ${ firstName }."); // My name is ${ firstName }.
+console.log("My name is ${firstName}."); // My name is ${ firstName }.
+// Using backticks as delimiter works the same, but allows for interpolation with template literals (${firstName} is a variable expression)
 
-// Using backticks as delimiter works the same, but allows for interpolation with template literals
+// Other primitive literals - booleans and numbers
+while (false) {
+  console.log(3.141592);
+}
+// boolean as the condition for the while loop, number as the value printed to the console
+// will never print because always false; a condition of true would print pi forever
