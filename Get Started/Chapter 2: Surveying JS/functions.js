8fbf791071f43a18fbef9f4fdf5842d6f8d52fc1
thaddeusrayd <thaddeusrayd@gmail.com>
2022-02-11 15:55:08 -0600

    defines functions

--- Get Started/Chapter 2: Surveying JS/functions.js
+++ Get Started/Chapter 2: Surveying JS/functions.js
@@ -1,1 +1,6 @@
 "use strict";
+
+// "functions" are "procedures"
+// procedure - collection of statements that can be invoked one or more times, may be provided some inputs,
+// and may give back one or more inputs
+//
