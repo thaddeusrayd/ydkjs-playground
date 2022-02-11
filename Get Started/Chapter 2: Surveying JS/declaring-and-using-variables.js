156d28522461a4de5c124f0f24c9d71113250431
thaddeusrayd <thaddeusrayd@gmail.com>
2022-02-11 15:40:49 -0600

    adds const section to variables file

--- Get Started/Chapter 2: Surveying JS/declaring-and-using-variables.js
+++ Get Started/Chapter 2: Surveying JS/declaring-and-using-variables.js
@@ -39,3 +39,25 @@ console.log(age); // Error // (commented out for section's terminal output)
 // very useful for limiting how widespread variable declarations are in our programs,
 // which helps prevent accidental overlap of their names.
 // var is still useful, however, in that it communicates that the variable will be seen outside of its scope
+// it's common to suggest that var should be thrown out since we have let and const (incoming),
+// but the author believes this to be the overly restrictive result of a misunderstanding of scoping behavior
+// and advocates that all available features be used when appropriate.
+
+// const
+// const works like let, but it MUST be a assigned a value at declaration and cannot be reassigned
+const myBirthday = true;
+let ageConstEx = 28;
+
+if (myBirthday) {
+  ageConstEx = ageConstEx + 1; // (this is fine)
+  // myBirthday = false; // (this is not - consts cannot be reassigned) // (commented out for section terminal output)
+}
+// const variables are not unchangeable; they just can't be reassigned
+// for this reason, it's not a great idea to assign object values to const variables
+// Example:
+const actors = ["Morgan Freeman", "Jennifer Anniston"];
+
+actors[2] = "Tom Cruise"; // (this technically works)
+// actors = []; // (this does not) // (commented out for terminal output)
+// best semantic use for const is with primitive values, as with myBirthday above
+// summation = const variables can be "mutated" but cannot be "reassigned" - to avoid confusion, use primitives
