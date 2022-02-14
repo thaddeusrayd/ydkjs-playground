62d01f0b486ac918808e98a9261043487f4c6ae4
thaddeusrayd <thaddeusrayd@gmail.com>
2022-02-14 14:31:08 -0600

    adds strict equality section to comparisons file

--- Get Started/Chapter 2: Surveying JS/comparisons.js
+++ Get Started/Chapter 2: Surveying JS/comparisons.js
@@ -1,1 +1,22 @@
 "use strict";
+
+// equality vs. equivalency
+// === means 'strict equality' -- MOST of the time, it indicates exact sameness
+
+3 === 3.0; // true
+"yes" === "yes"; // true
+null === null; // true
+false === false; // true
+
+42 === "42"; // false
+"hello" === "Hello"; // false
+true === 1; // false
+0 === null; // false
+"" === null; // false
+null === undefined; // false
+
+// === does not allow type coercion, while others do
+// === "lies" in two special cases: NaN and -0
+
+NaN === NaN; // false
+0 === -0; // true
