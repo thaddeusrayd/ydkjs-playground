fff8d19f9ab554fd341786b9ad3319d6386206ee
thaddeusrayd <thaddeusrayd@gmail.com>
2022-02-14 15:09:18 -0600

    adds intro to organization file

--- Get Started/Chapter 2: Surveying JS/how-we-organize-in-js.js
+++ Get Started/Chapter 2: Surveying JS/how-we-organize-in-js.js
@@ -1,1 +1,8 @@
 "use strict";
+
+// two major patterns for organizing code in JS: classes and modules
+// not mutually exclusive -- many programs use one, both, or neither
+// these patterns are very different, but they are also two sides of the same coin
+
+//
+// Classes
