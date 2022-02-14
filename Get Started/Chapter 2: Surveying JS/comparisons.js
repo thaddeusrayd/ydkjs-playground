c59a3df48612a475a5253b7b662b13741ad609c3
thaddeusrayd <thaddeusrayd@gmail.com>
2022-02-14 15:05:17 -0600

    adds coercive equality section to comparisons file

--- Get Started/Chapter 2: Surveying JS/comparisons.js
+++ Get Started/Chapter 2: Surveying JS/comparisons.js
@@ -43,3 +43,35 @@ x === [1,2,3] // false
 // x/y !== [1,2,3] because both x and y are being compared to a *new* [1,2,3]
 // content doesn't matter -- just the *identity*
 // JS does not support structural equality for object values, only reference identity comparison
+
+//
+// Coercive Comparisons
+// coercion is when a value of one type is converted into its respective representation in another type
+// core pillar of JS
+
+// == ("loose equality")
+// == works the same as === when the values have the same type, but it differs in that it allows coercion *first*
+// a better term = "coercive equality"
+
+42 == "42" // true
+1 == true // true
+// "42" and true are converted to their number values: 42 and 1
+// NOTE: == prefers primitive number comparisons
+// Problem with just using === all the time to avoid coercion comparison: relational comparison (>, <, <=, >=) also allow coercion
+// Example:
+var arr = ["1", "10", "100", "1000"];
+for (let i = 0; i<arr.length && arr[i] > 500; i++) {
+    // will run three times
+}
+// i<arr.length is safe from coercion because i and arr.length are always numbers
+// arr[i] > 500, however, is not. Because all of the array items are strings.
+// the loop stops at "1000" because the item is coerced to its number representation (1000) and compared to 500
+
+// relational operators typically use numeric comparisons, unless the items being compared are already strings, 
+// in which case they use alphabetical (dictionary-like) comparison of strings:
+x = "10"
+y = "9"
+
+x < y // true // "10" character code in ASCII is less than "9" character code
+
+
