1844997d8b9a0028b0024fb9648389a7603ce276
thaddeusrayd <thaddeusrayd@gmail.com>
2022-02-18 16:11:00 -0600

    adds closure intro

--- Get Started/Chapter 3: Digging to the Roots of JS/closure.js
+++ Get Started/Chapter 3: Digging to the Roots of JS/closure.js
@@ -1,3 +1,10 @@
 "use strict";
 
-//
+// Closure
+// fundamental and pervasive - VERY important to understand
+// defined - when a function remembers and continues to access variables from outside its scope,
+//  even when the function is executed in a different scope
+// two important characteristics:
+// 1. closure is part of the nature of a function -- objects don't get closures, functions do
+// 2. to observe a closure, you must execute a function in a different scope than where that function was initially defined
+// Example:
