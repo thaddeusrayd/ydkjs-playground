6d3b893093cc7fb7c876bf4b4b1bcd7867137efa
thaddeusrayd <thaddeusrayd@gmail.com>
2022-02-18 16:13:57 -0600

    adds closure example

--- Get Started/Chapter 3: Digging to the Roots of JS/closure.js
+++ Get Started/Chapter 3: Digging to the Roots of JS/closure.js
@@ -8,3 +8,15 @@
 // 1. closure is part of the nature of a function -- objects don't get closures, functions do
 // 2. to observe a closure, you must execute a function in a different scope than where that function was initially defined
 // Example:
+function greeting(msg) {
+  return function who(name) {
+    console.log(`${msg}, ${name}!`);
+  };
+}
+
+var hello = greeting("Hello");
+var howdy = greeting("Howdy");
+
+hello("Kyle"); // Hello, Kyle!
+hello("Sarah"); // Hello, Sarah!
+howdy("Grant"); // Howdy, Grant!
