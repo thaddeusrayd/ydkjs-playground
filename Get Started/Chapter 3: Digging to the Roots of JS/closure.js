18198a36d45ccac7c3225915776f86d318403757
thaddeusrayd <thaddeusrayd@gmail.com>
2022-02-19 15:50:02 -0600

    adds explanation of example and introduces closure modifications

--- Get Started/Chapter 3: Digging to the Roots of JS/closure.js
+++ Get Started/Chapter 3: Digging to the Roots of JS/closure.js
@@ -20,3 +20,29 @@ var howdy = greeting("Howdy");
 hello("Kyle"); // Hello, Kyle!
 hello("Sarah"); // Hello, Sarah!
 howdy("Grant"); // Howdy, Grant!
+// the greeting() outer function is called first, which creates an instance of the inner function who()
+// who() closes over the variable msg, which is the parameter of the outer scope of greeting()
+// when who() returns, its reference is assigned to the hello variable in the outer scope
+// then we call greeting() a second time, creating a new inner function instance,
+//  with a new closure over msg, which returns that reference to be assigned to howdy()
+
+// when greeting() finishes running, msg doesn't go away, because of closure
+// the msg variables persist because the inner function *instances* are assigned to hello and howdy - closure.
+// these closures are not a snapshot of the msg variable's value; they are a direct link and preservation of the variable itself
+// because of this, closures can observe or make changes to these variables over time:
+function counter(step = 1) {
+  var count = 0;
+  return function increaseCount() {
+    count = count + step;
+    return count;
+  };
+}
+
+var incBy1 = counter(1);
+var incBy3 = counter(3);
+
+console.log(incBy1()); // 1
+console.log(incBy1()); // 2
+console.log(incBy3()); // 3
+console.log(incBy3()); // 6
+console.log(incBy3()); // 9
