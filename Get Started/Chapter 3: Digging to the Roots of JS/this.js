aa453607bdebc4dc6477ebd2ab8bf3540d7af48e
thaddeusrayd <thaddeusrayd@gmail.com>
2022-02-25 17:07:43 -0600

    adds example of execution context to <this> section

--- Get Started/Chapter 3: Digging to the Roots of JS/this.js
+++ Get Started/Chapter 3: Digging to the Roots of JS/this.js
@@ -1,5 +1,3 @@
-"use strict";
-
 // <<this>> - wildly powerful, wildly misunderstood
 // a function's <<this>> does NOT refer to the function itself
 // not does <<this>> point to the instance that a method belongs to
@@ -15,3 +13,23 @@
 
 // <<this>> is not a fixed characteristic of a function based on function definition, but a *dynamic* characteristic that's
 //  determined each time the function is *called*
+
+// one way to think about execution context:
+// a tangible object whose properties are made available to a function *while it executes*
+// compared to scope, also an object, except the scope object is *hidden inside the JS engine*
+// thus, a scope is always the same for that function, and its props take the form of identifier variables inside the function:
+function classroom(teacher) {
+  return function study() {
+    console.log(`${teacher} says to study ${this.topic}`);
+  };
+}
+
+var assignment = classroom("Kyle");
+// the outer function, classroom(), makes no reference to a <<this>>, so it's just like any other function we've seen
+// however, the inner function, study(), does reference a <<this>>, making it a "this-aware" function,
+//  meaning it's dependent on its execution context
+// NOTE: study() also closes over the <<teacher>> variable from the outer scope
+assignment(); // Kyle says to study undefined // if using strict mode, it'll just error out
+// assignment() is called as a normal function with no execution context
+// without strict mode, context-aware functions like these default to the context of the global object (window in the browser)
+// since there is no global <<topic>> variable, this.topic resolves to undefined
