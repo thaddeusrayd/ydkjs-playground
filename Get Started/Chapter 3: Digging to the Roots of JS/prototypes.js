881acdfdea70b043eedb1e97d97d6d13513c79f4
thaddeusrayd <thaddeusrayd@gmail.com>
2022-03-16 23:58:50 -0500

    adds <<this>> revisited section

--- Get Started/Chapter 3: Digging to the Roots of JS/prototypes.js
+++ Get Started/Chapter 3: Digging to the Roots of JS/prototypes.js
@@ -47,3 +47,21 @@ console.log(homework.topic); // JS
 
 //
 // <<this>> revisited
+// true importance shines when considering how it powers prototype-delegated function calls
+// one of the main reasons it supports dynamic context based on how the function is called
+//  is so that method calls on objects which delegate through the prototype chain still
+//  maintain the expected <<this>>
+// consider:
+var homework = {
+  study() {
+    console.log(`Please study ${this.topic}`);
+  },
+};
+
+var jsHomework = Object.create(homework);
+jsHomework.topic = "JS";
+jsHomework.study(); // Please study JS
+
+var mathHomework = Object.create(homework);
+mathHomework.topic = "Math";
+mathHomework.study(); // Please study Math
