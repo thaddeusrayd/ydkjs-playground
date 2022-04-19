7685d5f1afbc757f995f8b8df7439c3d3421da46
thaddeusrayd <thaddeusrayd@gmail.com>
2022-04-19 11:05:05 -0500

    add notes to prototype section

---
+++ Get Started/Chapter 4: The Bigger Picture/pillar-two-prototypes.js
@@ -0,0 +1,14 @@
+/*
+NOTES
+
+JS is one of very few languages that allow you to create objects directly and explicitly without first 
+defining its structure in a class
+
+Before ES6 and its <class> keyword, people implemented class design pattern on top of prototypes - 
+"prototypal inheritance"
+
+Letting objects act as objects and cooperate with each other through the prototype chain is called "behavior
+delegation" or "object delegation", and the author argues that it's more powerful and "with the grain" of JS 
+than classes
+
+*/
