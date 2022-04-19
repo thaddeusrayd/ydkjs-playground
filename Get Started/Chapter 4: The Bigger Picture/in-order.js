9c4e1d3cda771562b4cc06130f07e0c93c03eff2
thaddeusrayd <thaddeusrayd@gmail.com>
2022-04-19 11:20:10 -0500

    add notes to in order section

---
+++ Get Started/Chapter 4: The Bigger Picture/in-order.js
@@ -0,0 +1,12 @@
+/*
+NOTES
+
+work through the series in order
+
+1. This one
+2. Scope and Closures - lexical scope, how it supports closure, how module pattern organizes code
+3. Objects and Classes - <this>, object delegation, <class>
+4. Types and Grammar - types and type coercion, syntax and grammar
+5. Sync and Async - flow control to model state change synchronously and asynchronously
+6. ES.Next and Beyond - future features and changes
+*/
