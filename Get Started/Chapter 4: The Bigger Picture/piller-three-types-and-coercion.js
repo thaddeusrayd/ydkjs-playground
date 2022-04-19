a930ad150712e7229471d061add3920fa3590534
thaddeusrayd <thaddeusrayd@gmail.com>
2022-04-19 11:09:03 -0500

    add notes to pillar three

---
+++ Get Started/Chapter 4: The Bigger Picture/piller-three-types-and-coercion.js
@@ -0,0 +1,10 @@
+/*
+NOTES
+
+types are vastly misunderstood, and interest has shifted to "static typing" - TypeScript, etc.
+
+arguably most important pillar
+
+get deeply familiar with how JS handles types and coercion, *then* get into TypeScript/Flow
+
+*/
