d6c73d948481547df50ec36921ef9117a108d807
thaddeusrayd <thaddeusrayd@gmail.com>
2022-04-23 13:59:08 -0500

    example and explanation

--- Get Started/appA-exploring-further/values-v-refs.js
+++ Get Started/appA-exploring-further/values-v-refs.js
@@ -16,3 +16,22 @@ console.log(myName); // Frank
 console.log(yourName); // Kyle
 
 // yourName was unaffected by the reassignment because it holds a copy
+
+// with references, two or more variables point to the same value,
+//  so modifications to the shared value will be reflected in all of its references
+// only object values (arrays, objects, functions, etc.) are treated as references
+
+var myAddress = {
+  street: "123 JS Blvd",
+  city: "Austin",
+  state: "Texas",
+};
+
+var yourAddress = myAddress;
+console.log(yourAddress.street); // 123 JS Blvd
+
+// time to move!
+myAddress.street = "456 TS Ave";
+console.log(yourAddress.street); // 456 TS Ave
+
+// myAddress holds an object, so the value is held by reference, and yourAddress is a copy of that reference
