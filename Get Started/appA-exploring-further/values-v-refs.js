9c37ff922eb52cd8082623c6e457fff4da479921
thaddeusrayd <thaddeusrayd@gmail.com>
2022-04-23 13:41:18 -0500

    introduction and example

---
+++ Get Started/appA-exploring-further/values-v-refs.js
@@ -0,0 +1,7 @@
+// how primitives and objects are passed around
+// if you assign/pass a value itself, the value is copied:
+
+var myName = "Kyle";
+var yourName = myName;
+
+// yourName has a separate copy of the string "Kyle" from the value stored in myName
