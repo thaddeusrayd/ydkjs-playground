36e09d771427ac6bbe55c6e7e756321dd544ce9a
thaddeusrayd <thaddeusrayd@gmail.com>
2022-04-23 14:22:03 -0500

    adds other declaration forms

--- Get Started/appA-exploring-further/so-many-function-forms.js
+++ Get Started/appA-exploring-further/so-many-function-forms.js
@@ -42,3 +42,13 @@ awesomeFunction.name; // "someName"
 // Other Function Declaration Forms
 
 // generator function declaration:
+function* two() {}
+
+// async function declaration:
+async function three() {}
+
+// async generator function declaration:
+async function* four() {}
+
+// named function export declaration (ES6 modules):
+export function five() {}
