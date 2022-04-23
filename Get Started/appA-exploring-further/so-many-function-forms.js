5aa11f035ae327e07d73e5755839965c8f3e2c6d
thaddeusrayd <thaddeusrayd@gmail.com>
2022-04-23 14:27:29 -0500

    adds other function expression forms

--- Get Started/appA-exploring-further/so-many-function-forms.js
+++ Get Started/appA-exploring-further/so-many-function-forms.js
@@ -52,3 +52,30 @@ async function* four() {}
 
 // named function export declaration (ES6 modules):
 export function five() {}
+
+//
+// Other Function Expression Forms
+
+// IIFE
+(function () {})();
+(function namedIIFE() {})();
+
+// async IIFE
+(async function () {})();
+(async function namedAIIFE() {})();
+
+// arrow function expressions
+var f;
+f = () => 42;
+f = (x) => x * 2;
+f = (x) => x * 2;
+f = (x, y) => x * y;
+f = (x) => ({ x: x * 2 });
+f = (x) => {
+  return x * 2;
+};
+f = async (x) => {
+  var y = await doSomethingAsync(x);
+  return y * 2;
+};
+someOperation((x) => x * 2);
