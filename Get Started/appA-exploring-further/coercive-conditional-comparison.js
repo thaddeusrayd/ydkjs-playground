b1a5954fa0331eda067fdb082bb34c658f72a49b
thaddeusrayd <thaddeusrayd@gmail.com>
2022-04-25 07:10:23 -0500

    all notes

--- Get Started/appA-exploring-further/coercive-conditional-comparison.js
+++ Get Started/appA-exploring-further/coercive-conditional-comparison.js
@@ -2,3 +2,48 @@
 
 // if/ternary statements and while/for loops perform implicit value comparisons,
 //  both "strictly" and "coercively"
+
+var x = 1;
+
+if (x) {
+  // will run!
+}
+
+while (x) {
+  // will run once!
+  x = false;
+}
+
+// you *could* think of these expressions like this:
+if (x == true) {
+  // will run!
+}
+
+while (x == true) {
+  // will run once!
+  x = false;
+}
+
+// however, this mental model doesn't work more broadly than this case
+// consider:
+var x = "hello";
+
+if (x) {
+  // will run!
+}
+
+if (x == true) {
+  // will not run!
+}
+
+// but why? here's the more accurate mental model:
+if (Boolean(x) == true) {
+  // will run!
+}
+// which is the same as...
+if (Boolean(x) === true) {
+  // will run!
+}
+
+// since Boolean() always returns a boolean value, == and === are irrelevant
+// the important thing to see is that coercion occurs before the comparison, from x's type to boolean
