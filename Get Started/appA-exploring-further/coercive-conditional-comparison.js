17706a4fa4891af15932a47d9182474914a29855
thaddeusrayd <thaddeusrayd@gmail.com>
2022-04-25 07:04:17 -0500

    intro

---
+++ Get Started/appA-exploring-further/coercive-conditional-comparison.js
@@ -0,0 +1,4 @@
+// conditional comparisons needing to make coercion-oriented comparisons
+
+// if/ternary statements and while/for loops perform implicit value comparisons,
+//  both "strictly" and "coercively"
