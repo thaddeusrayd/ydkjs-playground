1b88c1153f7cdb24f1ad5f5ecce91fe1ea4f01ba
thaddeusrayd <thaddeusrayd@gmail.com>
2022-04-26 14:14:26 -0500

    add suggested solution

--- Get Started/appB-practice/practicing-comparisons.js
+++ Get Started/appB-practice/practicing-comparisons.js
@@ -38,4 +38,44 @@ scheduleMeeting("17:00", 45); // true
 scheduleMeeting("17:30", 30); // false
 scheduleMeeting("18:00", 15); // false
 
-// recommended solutions:
+// suggested solution:
+
+function scheduleMeeting(startTime, durationMinutes) {
+  var [, meetingStartHour, meetingStartMinutes] =
+    startTime.match(/^(\d{1,2}):(\d{2})$/) || [];
+
+  durationMinutes = Number(durationMinutes);
+
+  if (
+    typeof meetingStartHour == "string" &&
+    typeof meetingStartMinutes == "string"
+  ) {
+    let durationHours = Math.floor(durationMinutes / 60);
+    durationMinutes = durationMinutes - durationHours * 60;
+    let meetingEndHour = Number(meetingStartHour) + durationHours;
+    let meetingEndMinutes = Number(meetingStartMinutes) + durationMinutes;
+
+    if (meetingEndMinutes >= 60) {
+      meetingEndHour = meetingEndHour + 1;
+      meetingEndMinutes = meetingEndMinutes - 60;
+    }
+
+    // re-compose fully-qualified time strings
+    // (to make comparison easier)
+    let meetingStart = `${meetingStartHour.padStart(
+      2,
+      "0"
+    )}:${meetingStartMinutes.padStart(2, "0")}`;
+    let meetingEnd = `${String(meetingEndHour).padStart(2, "0")}:${String(
+      meetingEndMinutes
+    ).padStart(2, "0")}`;
+
+    // NOTE: since expressions are all strings,
+    // comparisons here are alphabetic, but it's
+    // safe here since they're fully qualified
+    // time strings (ie, "07:15" < "07:30")
+    return meetingStart >= dayStart && meetingEnd <= dayEnd;
+  }
+
+  return false;
+}
