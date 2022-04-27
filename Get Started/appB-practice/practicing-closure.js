745d201861376bba0e235281d5f81764db844407
thaddeusrayd <thaddeusrayd@gmail.com>
2022-04-27 08:14:24 -0500

    challenge setup

---
+++ Get Started/appB-practice/practicing-closure.js
@@ -0,0 +1,24 @@
+/*
+
+Now let's practice with closure (Chapter 4, Pillar 1).
+
+The range(..) function takes a number as its first argument, representing the first number in a desired range of numbers. The second argument is also a number representing the end of the desired range (inclusive). If the second argument is omitted, then another function should be returned that expects that argument.
+
+*/
+
+function range(start, end) {
+  // ..TODO..
+}
+
+range(3, 3); // [3]
+range(3, 8); // [3,4,5,6,7,8]
+range(3, 0); // []
+
+var start3 = range(3);
+var start4 = range(4);
+
+start3(3); // [3]
+start3(8); // [3,4,5,6,7,8]
+start3(0); // []
+
+start4(6); // [4,5,6]
